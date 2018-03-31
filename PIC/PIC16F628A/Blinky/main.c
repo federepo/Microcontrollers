31a3db0bbbf6a6d195a7cfbd8d1ad29fdc2c8f17
federepo <32501268+federepo@users.noreply.github.com>
2018-03-31 13:16:52 -0300

    Update main.c

--- PIC/PIC16F628A/Blinky/main.c
+++ PIC/PIC16F628A/Blinky/main.c
@@ -1,1 +1,52 @@
 
+// PIC16F628A Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = INTOSCCLK // Oscillator Selection bits (INTOSC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is digital input, MCLR internally tied to VDD)
+#pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
+#pragma config LVP = ON         // Low-Voltage Programming Enable bit (RB4/PGM pin has PGM function, low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Data memory code protection off)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+#define _XTAL_FREQ 4000000 // Indicamos a que frecuencia de reloj esta funcionando el micro
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+//***************************
+//Ejemplo para el PIC16F628A
+//***************************
+
+#include <xc.h>
+
+int main(){
+    
+     //************************************************
+    //Configuración de puertos como entrada o salida
+    //************************************************
+    
+    TRISBbits.TRISB3 = 0; // Se configura el bit RC 2 como salida
+    // TRISC = 0b00000000; // Se configura a todos los bits del puerto C, como salidas
+     
+    //******************************
+    // Envío de datos a los puertos
+    //******************************
+     while (1) // Bucle infinito
+     {
+       //  PORTB = 0b00000000; // Se envía el dato 0X00 a todo el puerto B
+         PORTBbits.RB3 = 0; // Sen envía el dato "0" al bit RB3
+         __delay_ms(50);
+         
+       //  PORTB = 0b11111111; // Se envía el dato 0XFF a todo el puerto B
+         PORTBbits.RB3 = 1; // Sen envía el dato "1" al bit RB3
+         __delay_ms(50);
+    
+    }
+    
+    return 0;
+}
