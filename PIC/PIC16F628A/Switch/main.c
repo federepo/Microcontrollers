c3dd697cd8e6fa2633aa552f4b1f4c0be1054495
fgdangiolo <nubeingenieria@gmail.com>
2018-04-12 01:50:35 -0300

    subido

---
+++ PIC/PIC16F628A/Switch/main.c
@@ -0,0 +1,49 @@
+/****************************************************************
+Example: Switch
+Microcontroller: PIC16F628A
+****************************************************************/
+
+// PIC16F628A Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = INTOSCIO  // Oscillator Selection bits (INTOSC oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is digital input, MCLR internally tied to VDD)
+#pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
+#pragma config LVP = ON         // Low-Voltage Programming Enable bit (RB4/PGM pin has PGM function, low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Data memory code protection off)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+#define _XTAL_FREQ 4000000 // Clock frequency
+
+#include <xc.h>
+
+
+void main()
+{
+
+     //*********************
+    //Port configuration
+    //*********************
+    
+    TRISAbits.TRISA1 = 1;    // RA1 bit as input
+    TRISAbits.TRISA2 = 0;    // RA2 bit as output
+  
+     //************************
+    // Working on the ports
+    //************************
+    
+    while(1){     // Loop
+        
+      if(PORTAbits.RA1==1)    // RA1 es on?
+        {
+          PORTAbits.RA2 = !PORTAbits.RA2;  // Change the state
+       }
+
+    }
+
+    return;
+}
